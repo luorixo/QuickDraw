538dbdb31a8e1ea23e3f3729fca3333e3b60f4a4
Gauri <gauriraj.nz@gmail.com>
2022-09-19 16:13:09 +1200

    Fixed css file so that now buttons on the stats scene will get a drop shadow and scale larger when being hovered on. Also added in scale larger for the logo hover.

--- src/main/java/nz/ac/auckland/se206/StatsPageController.java
+++ src/main/java/nz/ac/auckland/se206/StatsPageController.java
@@ -1,3 +1,9 @@
 package nz.ac.auckland.se206;
 
-public class StatsPageController {}
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class StatsPageController {
+
+  @FXML private Label labelLogo;
+}
