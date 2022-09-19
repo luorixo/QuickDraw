82b16ae508473b6c2bd1577162cd87e67af9b618
Gauri <gauriraj.nz@gmail.com>
2022-09-19 17:07:45 +1200

    Fixed bug in User class where average Time would cause an error if calculated when 0 games have been played. Otherwise functionality of the statspageController have been added. All functionality is complete and working except the back button which needs to add userHome scene canvas name but the scene has not yet been created.

--- src/main/java/nz/ac/auckland/se206/StatsPageController.java
+++ src/main/java/nz/ac/auckland/se206/StatsPageController.java
@@ -1,9 +1,54 @@
 package nz.ac.auckland.se206;
 
+import java.io.IOException;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 
 public class StatsPageController {
 
-  @FXML private Label labelLogo;
+  @FXML private ImageView labelLogo;
+  @FXML private Button buttonBack;
+  @FXML private Label labelGamesWon;
+  @FXML private Label labelGamesPlayed;
+  @FXML private Label labelGamesLost;
+  @FXML private Label labelBestTime;
+  @FXML private Label labelAverageTime;
+  public static int id;
+  private User user;
+
+  /**
+   * On Scene intialisation set all of the label values to the respective users statistics. This
+   * assumes that the static variable of the user id has been set prior to the scene being created
+   * and called.
+   */
+  public void initialize() {
+    user = User.getUser(id); // get the user then set all labels
+    labelGamesWon.setText(String.valueOf(user.getGamesWon()));
+    labelGamesPlayed.setText(String.valueOf(user.getGamesPlayed()));
+    labelGamesLost.setText(String.valueOf(user.getGamesLost()));
+    labelBestTime.setText(String.valueOf(user.getBestTime()));
+    labelAverageTime.setText(String.valueOf(user.getAverageTime()));
+  }
+
+  /**
+   * On back button click this will set the scene back to the user home
+   *
+   * @param event
+   */
+  @FXML
+  public void onBackButton(ActionEvent event) {
+    Button button = (Button) event.getSource();
+    Scene currentScene = button.getScene();
+
+    try {
+      // change scene from stats page back to user home
+      currentScene.setRoot(App.loadFxml(""));
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
 }
