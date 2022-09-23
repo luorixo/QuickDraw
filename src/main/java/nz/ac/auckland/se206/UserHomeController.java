4048dc9b0689c88e6b7ec14462ccad4ed1048a67
Gauri <gauriraj.nz@gmail.com>
2022-09-24 00:45:03 +1200

    User Home fxml scene created. All visual content created - including final project design, however unimplemented game play buttons are currently disabled. Base controller class has been created and all resource images have been added.

---
+++ src/main/java/nz/ac/auckland/se206/UserHomeController.java
@@ -0,0 +1,24 @@
+package nz.ac.auckland.se206;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
+public class UserHomeController {
+
+  public static int id;
+  @FXML private Label nameLabel;
+  @FXML private Button backButton;
+  @FXML private ImageView logoImage;
+  @FXML private Button leftButton;
+  @FXML private Button rightButton;
+  @FXML private ImageView drawingImage;
+  @FXML private Label drawingLabel;
+  @FXML private Button zenButton;
+  @FXML private Button definitionButton;
+  @FXML private Button easyButton;
+  @FXML private Button mediumButton;
+  @FXML private Button hardButton;
+  @FXML private Button masterButton;
+}
