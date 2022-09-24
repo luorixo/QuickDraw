f888819cd5e9471d8d8e7070ed2c3ae32cd60cc5
Avinaash <akri235@aucklanduni.ac.nz>
2022-09-24 17:05:11 +1200

    commit to stop merge conflict
    had a merge conflict with main, needed to change the landing page
    controller and remove test info

--- src/main/java/nz/ac/auckland/se206/LandingPageController.java
+++ src/main/java/nz/ac/auckland/se206/LandingPageController.java
@@ -1,7 +1,8 @@
 package nz.ac.auckland.se206;
 
-import ai.djl.ModelException;
 import java.io.IOException;
+
+import ai.djl.ModelException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
@@ -10,27 +11,30 @@ import javafx.scene.control.Button;
 /** */
 public class LandingPageController {
 
-  @FXML private Button startButton;
+	@FXML
+	private Button startButton;
 
-  /**
-   * JavaFX calls this method once the GUI elements are loaded. In our case we create a listener for
-   * the drawing, and we load the ML model.
-   *
-   * @throws ModelException If there is an error in reading the input/output of the DL model.
-   * @throws IOException If the model cannot be found on the file system.
-   */
-  public void initialize() {}
+	/**
+	 * JavaFX calls this method once the GUI elements are loaded. In our case we
+	 * create a listener for the drawing, and we load the ML model.
+	 *
+	 * @throws ModelException If there is an error in reading the input/output of
+	 *                        the DL model.
+	 * @throws IOException    If the model cannot be found on the file system.
+	 */
+	public void initialize() {
+	}
 
-  @FXML
-  private void onStartGame(ActionEvent event) {
-    Button button = (Button) event.getSource();
-    Scene currentScene = button.getScene();
+	@FXML
+	private void onStartGame(ActionEvent event) {
+		Button button = (Button) event.getSource();
+		Scene currentScene = button.getScene();
 
-    try {
-      // change scene from landing page to the canvas
-      currentScene.setRoot(App.loadFxml("userprofilescene"));
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
-  }
+		try {
+			// change scene from landing page to the canvas
+			currentScene.setRoot(App.loadFxml("canvas"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
