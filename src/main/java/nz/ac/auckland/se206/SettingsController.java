126c58b452ab4bce4e7a6d184a23a137970eb51d
Gauri <gauriraj.nz@gmail.com>
2022-10-09 14:24:43 +1300

    Skeleton code for left right button in settings controller completed. Logic is finished just waiting for json code update so to be able to have full functionality

--- src/main/java/nz/ac/auckland/se206/SettingsController.java
+++ src/main/java/nz/ac/auckland/se206/SettingsController.java
@@ -1,5 +1,6 @@
 package nz.ac.auckland.se206;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -33,5 +34,154 @@ public class SettingsController {
     // shorted tooltip delay
     ToolTipManager.sharedInstance().setInitialDelay(0);
     ToolTipManager.sharedInstance().setReshowDelay(0);
+
+    // get current chosen difficulty and display for each label
+    // get current option for music, sound and speech and display appropriate image
+  }
+
+  /*
+   * This function will allow the left button to change the difficulty of the game, both the appropriate display and the user json will be updated. This function will allow cycling between EASY MEDIUM HARD and MASTER.
+   */
+  @FXML
+  private void setLeftButtonFourDifficulty(ActionEvent event) {
+    Button leftButton = (Button) event.getSource();
+    String buttonName = leftButton.getId();
+    String difficulty = "";
+
+    // get current difficulty of relevant metric
+    switch (buttonName) {
+      case "wordsLeft":
+        // get words difficulty
+      case "confidenceLeft":
+        // get confidence difficulty
+      case "timeLeft":
+        // get time difficulty
+    }
+    // left cycle for button
+    switch (difficulty) {
+      case "EASY":
+        difficulty = "MASTER";
+      case "MEDIUM":
+        difficulty = "EASY";
+      case "HARD":
+        difficulty = "MEDIUM";
+      case "MASTER":
+        difficulty = "HARD";
+    }
+
+    // set user profile preference and display label the new difficulty
+    switch (buttonName) {
+      case "wordsLeft":
+        // set words difficulty
+        wordsLabel.setText(difficulty);
+      case "confidenceLeft":
+        // set confidence difficulty
+        confidenceLabel.setText(difficulty);
+      case "timeLeft":
+        // set time difficulty
+        timeLabel.setText(difficulty);
+    }
+  }
+
+  /*
+   * This function will allow the right button to change the difficulty of the game, both the appropriate display and the user json will be updated. This function will allow cycling between EASY MEDIUM HARD and MASTER.
+   */
+  @FXML
+  private void setRightButtonFourDifficulty(ActionEvent event) {
+    Button rightButton = (Button) event.getSource();
+    String buttonName = rightButton.getId();
+    String difficulty = "";
+
+    // get current difficulty of relevant metric
+    switch (buttonName) {
+      case "wordsRight":
+        // get words difficulty
+      case "confidenceRight":
+        // get confidence difficulty
+      case "timeRight":
+        // get time difficulty
+    }
+    // right cycle for button
+    switch (difficulty) {
+      case "EASY":
+        difficulty = "MEDIUM";
+      case "MEDIUM":
+        difficulty = "HARD";
+      case "HARD":
+        difficulty = "MASTER";
+      case "MASTER":
+        difficulty = "EASY";
+    }
+
+    // set user profile preference and display label the new difficulty
+    switch (buttonName) {
+      case "wordsRight":
+        // set words difficulty
+        wordsLabel.setText(difficulty);
+      case "confidenceRight":
+        // set confidence difficulty
+        confidenceLabel.setText(difficulty);
+      case "timeRight":
+        // set time difficulty
+        timeLabel.setText(difficulty);
+    }
+  }
+
+  /*
+   * This function will allow the left button to change the difficulty of the game, both the appropriate display and the user json will be updated. This function will allow cycling between EASY MEDIUM and HARD.
+   */
+  @FXML
+  private void setLeftButtonThreeDifficulty(ActionEvent event) {
+    Button leftButton = (Button) event.getSource();
+    String buttonName = leftButton.getId();
+    String difficulty = "";
+
+    if (buttonName.equals("accuracyLeft")) {
+      // get difficulty of accuracy
+    }
+    // left cycle for button
+    switch (difficulty) {
+      case "EASY":
+        difficulty = "HARD";
+      case "MEDIUM":
+        difficulty = "EASY";
+      case "HARD":
+        difficulty = "MEDIUM";
+    }
+
+    // set user profile preference and display label the new difficulty
+    if (buttonName.equals("accuracyLeft")) {
+      // set difficulty of accuracy
+      accuracyLabel.setText(difficulty);
+    }
+  }
+
+  /*
+   * This function will allow the right button to change the difficulty of the game, both the appropriate display and the user json will be updated. This function will allow cycling between EASY MEDIUM and HARD.
+   */
+  @FXML
+  private void setRightButtonThreeDifficulty(ActionEvent event) {
+    Button rightButton = (Button) event.getSource();
+    String buttonName = rightButton.getId();
+    String difficulty = "";
+
+    if (buttonName.equals("accuracyRight")) {
+      // get difficulty of accuracy
+    }
+    // left cycle for button
+    switch (difficulty) {
+      case "EASY":
+        difficulty = "MEDIUM";
+      case "MEDIUM":
+        difficulty = "HARD";
+      case "HARD":
+        difficulty = "EASY";
+    }
+
+    // set user profile preference and display label the new difficulty
+    if (buttonName.equals("accuracyRight")) {
+      // set difficulty of accuracy
+      accuracyLabel.setText(difficulty);
+    }
   }
 }
