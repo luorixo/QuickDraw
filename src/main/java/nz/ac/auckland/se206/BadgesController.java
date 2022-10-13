506b7ddc2510e785aa8bbb1ef5d7c6989edb1551
Gauri <gauriraj.nz@gmail.com>
2022-10-13 18:37:44 +1300

    Created fxml, css and controller for badges page. FXML and css are completed, adn all resources have been added. Controller is not complete

---
+++ src/main/java/nz/ac/auckland/se206/BadgesController.java
@@ -0,0 +1,11 @@
+package nz.ac.auckland.se206;
+
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+
+public class BadgesController {
+
+  @FXML private ImageView imageLogo;
+
+  public void initialize() {}
+}
