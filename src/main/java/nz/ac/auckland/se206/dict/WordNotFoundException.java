1db143329f2389d7225f9b7905df66b86852d4e6
luorixo <luorixo@gmail.com>
2022-10-15 20:54:12 +1300

    added a lot of javadocs to my classes

--- src/main/java/nz/ac/auckland/se206/dict/WordNotFoundException.java
+++ src/main/java/nz/ac/auckland/se206/dict/WordNotFoundException.java
@@ -6,16 +6,33 @@ public class WordNotFoundException extends Exception {
   private String word;
   private String subMessage;
 
+  /**
+   * WordNotFoundException handler constructor that takes in word, message, submessage
+   *
+   * @param word word
+   * @param message message
+   * @param subMessage submessage
+   */
   WordNotFoundException(String word, String message, String subMessage) {
     super(message);
     this.word = word;
     this.subMessage = subMessage;
   }
 
+  /**
+   * getter for the word in exception
+   *
+   * @return the word
+   */
   public String getWord() {
     return word;
   }
 
+  /**
+   * getter for the submessage in exception
+   *
+   * @return the submessage
+   */
   public String getSubMessage() {
     return subMessage;
   }
